1fc3ae2b84cc4ef56bd066b268ac1b625b3246e4
tanzila-abedin <tanzila.abedin@yahoo.com>
2021-11-12 06:58:28 +0200

    Create action creators to fetch movies and fetch movie data from two different apis

---
+++ src/Redux/actions/createSearch.js
@@ -0,0 +1,51 @@
+import axios from 'axios';
+import {
+  FETCH_MOVIES_FAILURE, FETCH_MOVIES_REQUEST, FETCH_MOVIES_SUCCESS, FETCH_MOVIE_DETAIL,
+} from './type';
+
+const fetchMoviesRequest = () => ({
+  type: FETCH_MOVIES_REQUEST,
+});
+
+const fetchMoviesSuccess = (movies) => ({
+  type: FETCH_MOVIES_SUCCESS,
+  payload: movies,
+});
+
+const fetchMovieDetail = (movie) => ({
+  type: FETCH_MOVIE_DETAIL,
+  payload: movie,
+});
+
+const fetchMoviesFailure = (error) => ({
+  type: FETCH_MOVIES_FAILURE,
+  payload: error,
+});
+
+const fetchMovies = () => (dispatch) => {
+  dispatch(fetchMoviesRequest);
+  const url = 'https://imdb-api.com/en/API/MostPopularMovies/k_saglkr13';
+  axios.get(url).then((response) => {
+    const movies = response.data.items;
+    dispatch(fetchMoviesSuccess(movies));
+  }).catch((error) => {
+    const errorMsg = error.message;
+    dispatch(fetchMoviesFailure(errorMsg));
+  });
+};
+
+const fetchMovie = (id) => (dispatch) => {
+  dispatch(fetchMoviesRequest);
+  const url = (`https://imdb-api.com/en/API/Ratings/k_saglkr13/${id}`);
+  axios.get(url).then((response) => {
+    const movie = response.data;
+    dispatch(fetchMovieDetail(movie));
+  }).catch((error) => {
+    const errorMsg = error.message;
+    dispatch(fetchMoviesFailure(errorMsg));
+  });
+};
+
+export {
+  fetchMoviesFailure, fetchMoviesSuccess, fetchMoviesRequest, fetchMovies, fetchMovie,
+};
