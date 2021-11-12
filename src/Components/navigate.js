3953ddec0e5b24ede65698b5bfc9caa9edc12445
tanzila-abedin <tanzila.abedin@yahoo.com>
2021-11-12 21:03:48 +0200

    Add BS classes to navbar texts

--- src/Components/navigate.js
+++ src/Components/navigate.js
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom';
 const Navigate = () => (
   <nav className="navHead navbar navbar-light">
     <div className="container">
-      <span className="navbar-brand mb-0 h1">Movie Mania</span>
+      <p className="navbar-brand mb-0 text-warning nav-text font-weight-normal">Movie Mania</p>
+      <Link className="nav-text font-weight-normal" to="/">Home</Link>
     </div>
-    <Link to="/">Home</Link>
   </nav>
 
 );
