d06a4240316f00787c3018ec82191bb2121cb351
tanzila-abedin <tanzila.abedin@yahoo.com>
2021-11-14 23:55:53 +0200

    Remove commented out code

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MovieList from '../Containers/MovieList';
-// import Header from './header';
 import Navigate from './navigate';
 
 import SingleMovie from '../Containers/singleMovie';
@@ -9,12 +8,9 @@ import Footer from './footer';
 function App() {
   return (
     <BrowserRouter>
-      {/* <Header /> */}
       <Navigate />
       <Routes>
-        <Route exact path="/" element={<MovieList />}>
-          {/* <MovieList /> */}
-        </Route>
+        <Route exact path="/" data-testid="movieList" element={<MovieList />} />
         <Route path="/singleMovie/:id" element={<SingleMovie />} />
       </Routes>
       <Footer />
